d6a961ac9616310a76c1ff3c8f202a1dde63a538
Cedyon <cedyon2016@gmail.com>
2023-03-02 14:22:29 -0800

    update

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - CAPITALIZE STRING
+ * @str: String be capitalized.
+ *
+ * Return: string
+ */
+char *cap_string(char *str)
+{
+	int j = 0;
+
+	while (str[j])
+	{
+		while (!(str[j] >= 'a' && str[j] <= 'z'))
+			j++;
+
+		if (str[j - 1] == ' ' ||
+		    str[j - 1] == '\t' ||
+		    str[j - 1] == '\n' ||
+		    str[j - 1] == ',' ||
+		    str[j - 1] == ';' ||
+		    str[j - 1] == '.' ||
+		    str[j - 1] == '!' ||
+		    str[j - 1] == '?' ||
+		    str[j - 1] == '"' ||
+		    str[j - 1] == '(' ||
+		    str[j - 1] == ')' ||
+		    str[j - 1] == '{' ||
+		    str[j - 1] == '}' ||
+		    j == 0)
+			str[j] -= 32;
+
+		j++;
+	}
+
+	return (str);
+}
